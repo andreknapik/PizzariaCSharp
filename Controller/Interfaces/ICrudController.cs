393fc3aef3b09d05869b02bc5318f175d535882a
Andre Knapik <andreluisknapik@gmail.com>
2022-07-08 11:38:35 -0300

    Finalizando o Projeto PizzariaCSharp

--- Controller/Interfaces/ICrudController.cs
+++ Controller/Interfaces/ICrudController.cs
@@ -5,12 +5,40 @@ using System.Threading.Tasks;
 
 namespace PizzariaCSharp.Controller.Interfaces
 {
+    /// <summary>
+    /// Interface genérica para os crus de controllers.
+    /// </summary>
+    /// <typeparam name="T">Modelo que será controlado</typeparam>
     public interface ICrudController<T>
     {
+    /// <summary>
+    /// Método para adicionar um modelo
+    /// </summary>
+    /// <param name="modelo">Modelo a ser adicionado</param>
+    /// <returns>Retorna o modelo adicionado</returns>
     T Adicionar(T modelo);
+    /// <summary>
+    /// Metodo para obter a lista de modelos
+    /// </summary>
+    /// <returns>Retorna a lista de modelos encontrados.</returns>
     List<T> ObterTodos();
+    /// <summary>
+    /// Metodo para encontrar o modelo seu Id
+    /// </summary>
+    /// <param name="id">Id do modelo a ser encontrato</param>
+    /// <returns>Retorna o modelo encontrado</returns>
     T Obter(int id);
+    /// <summary>
+    ///  Metodo para atualizar um modelo
+    /// </summary>
+    /// <param name="id">Id do modelo que será atualizado</param>
+    /// <param name="modelo">Modelo que será atualizado</param>
+    /// <returns>Retorna o modelo após a atualização.</returns>
     T Atualizar(int id, T modelo);
+    /// <summary>
+    /// Metodo para deletar o modelo pelo seu Id.
+    /// </summary>
+    /// <param name="id">id do modelo a ser deletado.</param>
     void Deletar(int id);
 
     }
--- Repository/Interfaces/ICrudRepository.cs
+++ Repository/Interfaces/ICrudRepository.cs
@@ -5,13 +5,43 @@ using System.Threading.Tasks;
 
 namespace PizzariaCSharp.Repository.Interfaces
 {
+    /// <summary>
+    /// Interface do repositório genérico baseado na construção do modelo.
+    /// </summary>
+    /// <typeparam name="T">Modelo, entidade a qual será persistida.</typeparam>
+
     public interface ICrudRepository<T>
     {        
+        /// <summary>
+        /// /Metodo que adiciona um modelo ao banco de dados.
+        /// </summary>
+        /// <param name="modelo">Modelo que será adicionado.</param>
+        /// <returns>Retorna o modelo após ser adicionado no banco de dados.</returns>
         T Adicionar(T modelo);
+        /// <summary>
+        /// Método que retorna uma lista com todos os modelos encontrados no banco de dados.
+        /// </summary>
+        /// <returns>retorna lista com todos os modelos.</returns>
         List<T> ObterTodos();
+        /// <summary>
+        /// Metodo que retorna o modelo enmcontrado pelo seu id.
+        /// </summary>
+        /// <param name="id">Id o modelo a ser encontrado</param>
+        /// <returns>Retorna o modelo encontrado pelo Id.</returns>
         T Obter(int id);
+        /// <summary>
+        /// Metodo para atualizar um modelo no banco de dados.
+        /// </summary>
+        /// <param name="modelo">Modelo a ser atualizado.</param>
+        /// <returns>Retorna o modelo após atualizaçao.</returns>
         T Atualizar(T modelo);
+        /// <summary>
+        /// Deleta o modelo pelo seu ID.
+        /// </summary>
+        /// <param name="id">Id do modelo que será deletado.</param>
         void Deletar(int id);
+    
+
 
     }
 }
